269b5f3089a77c6f15edad3f46ca4cbba864b093
Franco Ortega <francopdx@gmail.com>
2020-12-10 19:49:27 -0800

    add POST test to bird.test.js, add insert() to Bird model, add POST Bird endpoint to app.js

--- __tests__/birds.test.js
+++ __tests__/birds.test.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
-//const pool = require('..lib/utils/pool');
+const pool = require('../lib/utils/pool');
 
 describe('tests for app.js endpoints', () => {
+  beforeEach(() => pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8')));
+      
+  afterAll(() => pool.end());
+
 
   // Sample test
   it('sample test to check connection', () => {
@@ -13,6 +18,23 @@ describe('tests for app.js endpoints', () => {
       });
 
   });
+
+  // Bird tests: POST, GET all, GET one, PUT, DELETE
+  it('create a new bird with POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/birds')
+      .send({
+        birdSpecies: 'tiger owl',
+        color: 'red'
+      });
+  
+    expect(res.body).toEqual({
+      id: '1',
+      birdSpecies: 'tiger owl',
+      color: 'red'
+    });
+  });
+  
 
 
 
