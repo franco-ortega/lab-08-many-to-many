cad1a2b641def42178f378cab3214b06010e81d2
Franco Ortega <francopdx@gmail.com>
2020-12-10 18:26:29 -0800

    add GET all test to trees.test.js, add find() to Tree model, add GET all Tree endpoint to app.js

--- __tests__/trees.test.js
+++ __tests__/trees.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
-//const Tree = require('../lib/models/trees');
+const Tree = require('../lib/models/trees');
 
 
 describe('tests for app.js endpoints', () => {
@@ -21,6 +21,7 @@ describe('tests for app.js endpoints', () => {
 
   });
 
+  // Tree model tests: POST, GET all, GET one, PUT, DELETE
   it('create a new tree with POST', async() => {
     const res = await request(app)
       .post('/api/v1/trees')
@@ -28,12 +29,36 @@ describe('tests for app.js endpoints', () => {
         treeSpecies: 'oak',
         rings: 105
       });
-console.log(res.body);
+
     expect(res.body).toEqual({
       id: '1',
       treeSpecies: 'oak',
       rings: 105
     });
+
+  });
+
+  it('get all trees with GET', async() => {
+    const trees = await Promise.all([
+      {
+        treeSpecies: 'oak',
+        rings: 105
+      },
+      {
+        treeSpecies: 'redwood',
+        rings: 675
+      },
+      {
+        treeSpecies: 'elm',
+        rings: 49
+      }
+    ].map(tree => Tree.insert(tree)));
+
+    const res = await request(app)
+      .get('/api/v1/trees');
+
+    expect(res.body).toEqual(expect.arrayContaining(trees));
+    expect(res.body).toHaveLength(trees.length);
 
   });
 
