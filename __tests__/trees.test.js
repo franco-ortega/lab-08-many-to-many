2c68ea7c702d7b44726a503b05a9d43a37f47ce1
Franco Ortega <francopdx@gmail.com>
2020-12-10 19:20:31 -0800

    clean up code in trees.test.js and trees.js

--- __tests__/trees.test.js
+++ __tests__/trees.test.js
@@ -4,24 +4,12 @@ const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
 const Tree = require('../lib/models/trees');
 
-
 describe('tests for app.js endpoints', () => {
   beforeEach(() => pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8')));
       
   afterAll(() => pool.end());
 
-
-  // Sample test
-  it('sample test to check connection', () => {
-    return request(app)
-      .get('/')
-      .then(res => {
-        expect(res.body).toEqual({ climb: 'the trees' });
-      });
-
-  });
-
-  // Tree model tests: POST, GET all, GET one, PUT, DELETE
+  // Tree tests: POST, GET all, GET one, PUT, DELETE
   it('create a new tree with POST', async() => {
     const res = await request(app)
       .post('/api/v1/trees')
@@ -35,7 +23,6 @@ describe('tests for app.js endpoints', () => {
       treeSpecies: 'oak',
       rings: '105'
     });
-
   });
 
   it('get all trees with GET', async() => {
@@ -59,7 +46,6 @@ describe('tests for app.js endpoints', () => {
 
     expect(res.body).toEqual(expect.arrayContaining(trees));
     expect(res.body).toHaveLength(trees.length);
-
   });
 
   it('get one tree via GET', async() => {
@@ -75,7 +61,6 @@ describe('tests for app.js endpoints', () => {
 
     expect(res.body).toEqual(tree);
   });
-
 
   it('update one tree via PUT', async() => {
     const tree = await Tree.insert(
@@ -116,10 +101,5 @@ describe('tests for app.js endpoints', () => {
 
     expect(res.body).toEqual(tree);
   });
-
-
-
-
-
 
 });
