f648ffd7146b710276a0a0727393803b20094f19
Franco Ortega <francopdx@gmail.com>
2020-12-10 19:24:42 -0800

    uncomment tables code in setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS trees;
--- DROP TABLE IF EXISTS birds CASCADE;
--- DROP TABLE IF EXISTS trees_birds;
+DROP TABLE IF EXISTS trees CASCADE;
+DROP TABLE IF EXISTS birds CASCADE;
+DROP TABLE IF EXISTS trees_birds;
 
 CREATE TABLE trees (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,13 +8,13 @@ CREATE TABLE trees (
     rings INTEGER CHECK (rings > 0)
 );
 
--- CREATE TABLE birds (
---     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     bird_species TEXT NOT NULL,
---     color TEXT NOT NULL
--- );
+CREATE TABLE birds (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    bird_species TEXT NOT NULL,
+    color TEXT NOT NULL
+);
 
--- CREATE TABLE trees_birds (
---     tree_id BIGINT REFERENCES trees(id),
---     bird_id BIGINT REFERENCES birds(id)
--- );
+CREATE TABLE trees_birds (
+    tree_id BIGINT REFERENCES trees(id),
+    bird_id BIGINT REFERENCES birds(id)
+);
